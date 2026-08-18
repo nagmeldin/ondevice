86b728c17523172260ca430e46b8fdebeace14a7
nagmeldin <eldin.nagm@gmail.com>
2026-08-18 14:58:28 -0600

    Decorated Rank record as MappedEntity

--- src/main/java/org/engyne/Rank.java
+++ src/main/java/org/engyne/Rank.java
@@ -1,17 +1,17 @@
 package org.engyne;
 
+import io.micronaut.data.annotation.MappedEntity;
 import jakarta.validation.constraints.*;
 
+@MappedEntity
 public record Rank(
         @Positive
         @Min(1)
         @Max(5)
         @NotBlank
         int grade,
-
         @NotBlank
         Status status,
-
         @Size(max = 255)
         String note
 ) {
